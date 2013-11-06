f4ae5478778b82a392e63321d9429ed6817b440c
Florian Festi <ffesti@redhat.com>
2013-11-06 19:29:02 +0100

    Box.py: Remove bolt holes and set surroundingspace

--- box.py
+++ box.py
@@ -3,22 +3,24 @@
 from boxes import *
 
 class Box(Boxes):
-    def __init__(self, x, y, h):
+    def __init__(self, x, y, h, **kw):
         self.x, self.y, self.h = x, y, h
-        Boxes.__init__(self, width=x+y+40, height=y+2*h+50)
+        Boxes.__init__(self, width=x+y+40, height=y+2*h+50, **kw)
 
     def render(self):
         x, y, h = self.x, self.y, self.h
         t = self.thickness
 
-        d2 = Bolts(2)
-        d3 = Bolts(3)
+        d2 = [Bolts(2)]
+        d3 = [Bolts(3)]
+
+        d2 = d3 = None
 
         self.moveTo(t, t)
-        self.rectangularWall(x, h, "FfeF", bedBolts=[d2], move="right")
-        self.rectangularWall(y, h, "FfeF", bedBolts=[d3], move="up")
-        self.rectangularWall(y, h, "FfeF", bedBolts=[d3])
-        self.rectangularWall(x, h, "FfeF", bedBolts=[d2], move="left up")
+        self.rectangularWall(x, h, "FfeF", bedBolts=d2, move="right")
+        self.rectangularWall(y, h, "FfeF", bedBolts=d3, move="up")
+        self.rectangularWall(y, h, "FfeF", bedBolts=d3)
+        self.rectangularWall(x, h, "FfeF", bedBolts=d2, move="left up")
         
         self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3])
 
@@ -26,6 +28,7 @@ class Box(Boxes):
         self.surface.flush()
         self.surface.finish()
 
-b = Box(100, 150, 70)
-b.edges["f"].settings.setValues(b.thickness, space=3, finger=2)
+b = Box(200, 200, 200, thickness=4.0)
+b.edges["f"].settings.setValues(b.thickness, space=3, finger=3,
+                                surroundingspaces=1)
 b.render()

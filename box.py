82ebbf09ed6f5c49411ff1a06df2c1dd3328cbca
Florian Festi <ffesti@redhat.com>
2014-01-21 22:40:21 +0100

    box.py: Make sides symetric and add top

--- box.py
+++ box.py
@@ -17,18 +17,19 @@ class Box(Boxes):
         d2 = d3 = None
 
         self.moveTo(t, t)
-        self.rectangularWall(x, h, "FFeF", bedBolts=d2, move="right")
-        self.rectangularWall(y, h, "Ffef", bedBolts=d3, move="up")
-        self.rectangularWall(y, h, "Ffef", bedBolts=d3)
-        self.rectangularWall(x, h, "FFeF", bedBolts=d2, move="left up")
+        self.rectangularWall(x, h, "FFFF", bedBolts=d2, move="right")
+        self.rectangularWall(y, h, "FfFf", bedBolts=d3, move="up")
+        self.rectangularWall(y, h, "FfFf", bedBolts=d3)
+        self.rectangularWall(x, h, "FFFF", bedBolts=d2, move="left up")
         
+        self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3], move="right")
         self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3])
 
         self.ctx.stroke()
         self.surface.flush()
         self.surface.finish()
 
-b = Box(200, 200, 200, thickness=4.0)
+b = Box(140, 202, 50, thickness=4.0)
 b.edges["f"].settings.setValues(b.thickness, space=3, finger=3,
                                 surroundingspaces=1)
 b.render()

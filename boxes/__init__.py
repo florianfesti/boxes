6c82bb9e32595d330a5c3251f9d290481f80628a
Marius Bergmann <marius@yeai.de>
2016-07-11 23:01:07 +0200

    Use cairocffi instead of cairo - if available
    cairocffi is available from pip, which makes setup much easier.

--- boxes/__init__.py
+++ boxes/__init__.py
@@ -14,6 +14,11 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+try:
+    import cairocffi
+    cairocffi.install_as_pycairo()
+except ImportError:
+    pass
 import cairo
 import math
 import argparse

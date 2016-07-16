deb448202532880550a12196457cac6e9a15f2ba
bobm123 <bobm123@gmail.com>
2016-07-15 20:23:41 -0400

    A minimum viable 6-pack caddy.

--- boxes/generators/caddy.py
+++ boxes/generators/caddy.py
@@ -17,26 +17,27 @@
 from boxes import *
 
 
-class RoundedTriangleSettings(edges.Settings):
+class RoundedTriangleEdgeSettings(edges.Settings):
     absolute_params = {
         "angle" : 60,
-        "radius" : 30,
+        "radius" : 20,
         "r_hole" : None,
         }
 
-class RoundedTriangle(edges.Edge):
+class RoundedTriangleEdge(edges.Edge):
+    """Makes an 'edge' with a rounded triangular bumpout and
+       optional hole"""
     char = "q"
     def __call__(self, length, **kw):
-        self.corner(-90)
         angle = self.settings.angle
         r = self.settings.radius
-
-        if self.settings.r_hole:
-            x = 0.5*(length-2*r)*math.tan(math.radians(angle))
-            y =  0.5*(length)
-            self.hole(x, y, self.settings.r_hole)
-
+        x = 0.5 * (length-2*r) * math.tan(math.radians(angle))
+        y = 0.5 * (length)
         l = 0.5 * (length-2*r) / math.cos(math.radians(angle))
+
+        self.corner(-90)
+        if self.settings.r_hole:
+            self.hole(x, y, self.settings.r_hole)
         self.corner(90-angle, r)
         self.edge(l)
         self.corner(2*angle, r)
@@ -44,11 +45,17 @@ class RoundedTriangle(edges.Edge):
         self.corner(90-angle, r)
         self.corner(-90)
 
+    def margin(self):
+        #TODO: replace hardcoded value with x+r
+        return self.boxes.spacing + self.boxes.thickness + 120
+
 
 class Box(Boxes):
-    """Fully closed box"""
+    """An open box with two special 'house shaped' sides to make 6-pack 
+       style caddy. A dowel through the sides makes a handle"""
     def __init__(self):
         Boxes.__init__(self)
+        #TODO: Get hole, rounding radius and angle from command line
         self.buildArgParser("x", "y", "h", "outside")
         self.argparser.set_defaults(
             fingerjointfinger=3.0,
@@ -66,36 +73,26 @@ class Box(Boxes):
 
         self.open()
 
-        s = RoundedTriangleSettings(self.thickness, angle=60, r_hole=5)
-        self.addPart(RoundedTriangle(self, s))
+        s = RoundedTriangleEdgeSettings(self.thickness, angle=60, r_hole=5)
+        self.addPart(RoundedTriangleEdge(self, s))
 
-        # (bottom, right, top, left)
-
-        self.moveTo(t, t)
-        #self.rectangularWall(h, y, "FfFe", move="right")
-        #self.moveTo(h+2*t, 0)
-        self.rectangularWall(x, y, "FFFF", move="right")
-        #self.moveTo(x+3*t, 0)
-
-        self.rectangularWall(h, y, "FqFf", move="right")
-        self.moveTo(-(x+3*t), y+3*t)
-
-        self.rectangularWall(h, y, "FfFq", move="right")
-        self.moveTo(-(x+3*t), y+3*t)
-
-        self.rectangularWall(x, h, "ffef", move="up")
-        #self.moveTo(0, -(h+y+5*t))
-        #self.moveTo(0, (h+3*t))
-        self.rectangularWall(x, h, "efff")
+        # Notes on rectangularWall parameters, all reference a 2D part
+        #   screen width, or X-dimension
+        #   screen height, Y-dimension
+        #   edge defines the style of the 4 sides of athe 2D part 
+        #       (bottom, right, top, left) of 2D part. In general,
+        #        match F with f on mating edges of the box
+        #   move= turtle graphic commands that move origin drawing
+        #
+        # Arguments x, y, h are 3D parameters of the box we're making.
+        #
+        self.rectangularWall(h, y, "FfFq", move="right")    #handle end
+        self.rectangularWall(x, y, "FFFF", move="right")    #bottom
+        self.rectangularWall(h, y, "FqFf", move="up")       #handle end
+        self.rectangularWall(x, h, "ffef", move="up left")  #side
+        self.rectangularWall(x, h, "efff")                  #side
         
-        #self.moveTo(t, t)
-        #self.rectangularWall(x, h, "FFeF", bedBolts=d2)
-        #self.moveTo(x+3*t, 0)
-        #self.rectangularWall(y, h, "Feef", bedBolts=d3)
-        #self.moveTo(0, h+3*t)
-        #self.rectangularWall(y, h, "Feef", bedBolts=d3)
-        #self.moveTo(-x-3*t, 0)
-        #self.rectangularWall(x, y, "efff", bedBolts=[d2, d3, d2, d3])
+        #TODO: Add a slotted Insert
 
         self.close()
 

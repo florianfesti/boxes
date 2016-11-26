f30221d26535e28d11b4464f04b2a05a39c2bc6b
Florian Festi <ffesti@redhat.com>
2016-11-26 13:16:33 +0100

    Fix .margin() for CompondEdge

--- boxes/edges.py
+++ boxes/edges.py
@@ -373,7 +373,7 @@ class CompoundEdge(BaseEdge):
         return self.types[-1].endwidth()
 
     def margin(self):
-        return max((e.margin() for e in self.types))
+        return max((e.margin() + e.startwidth() for e in self.types)) - self.types[0].startwidth()
 
     def __call__(self, length, **kw):
         if length and abs(length - self.length) > 1E-5:

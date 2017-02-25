42ab40da86363a673874039a6be3088a7d573716
Florian Festi <florian@festi.info>
2017-02-25 20:01:03 +0100

    Build and ship inkscape inx files

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+import os
+import glob
+
+class CustomBuildExtCommand(build_py):
+    """Customized setuptools install command - prints a friendly greeting."""
+
+    def buildInkscapeExt(self):
+        os.system("%s %s" % (os.path.join("scripts", "boxes2inkscape"), "inkex"))
+
+    def run(self):
+        self.execute(self.buildInkscapeExt, ())
+        if os.name == "posix":
+            if self.distribution.data_files is None:
+                self.distribution.data_files = []
+            self.distribution.data_files.append(
+                ("/usr/share/inkscape/extensions/", [i for i in glob.glob(os.path.join("inkex", "*.inx"))]))
+        build_py.run(self)
 
 setup(name='boxes',
       version='0.1',
@@ -11,6 +29,9 @@ setup(name='boxes',
       packages=find_packages(),
       install_requires=['cairocffi'],
       scripts=['scripts/boxes', 'scripts/boxesserver'],
+      cmdclass={
+          'build_py': CustomBuildExtCommand,
+      },
       classifiers=[
           "Development Status :: 4 - Beta",
           "Environment :: Console",

b623008ea331b7ea1f730ac53e53ab6ade9eda05
Florian Festi <florian@festi.info>
2017-09-13 11:35:10 +0200

    ClosedBox: fix (commented out) BedBolt support

--- boxes/generators/closedbox.py
+++ boxes/generators/closedbox.py
@@ -39,15 +39,15 @@ class ClosedBox(Boxes):
 
         t = self.thickness
 
-        d2 = [edges.Bolts(2)]
-        d3 = [edges.Bolts(3)]
+        d2 = edges.Bolts(2)
+        d3 = edges.Bolts(3)
 
         d2 = d3 = None
 
-        self.rectangularWall(x, h, "FFFF", bedBolts=d2, move="right")
-        self.rectangularWall(y, h, "FfFf", bedBolts=d3, move="up")
-        self.rectangularWall(y, h, "FfFf", bedBolts=d3)
-        self.rectangularWall(x, h, "FFFF", bedBolts=d2, move="left up")
+        self.rectangularWall(x, h, "FFFF", bedBolts=[d2], move="right")
+        self.rectangularWall(y, h, "FfFf", bedBolts=[d3], move="up")
+        self.rectangularWall(y, h, "FfFf", bedBolts=[d3])
+        self.rectangularWall(x, h, "FFFF", bedBolts=[d2], move="left up")
 
         self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3], move="right")
         self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3])

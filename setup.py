fe61c06cfea987734c62a2e2a4251e97e220926a
Florian Festi <florian@festi.info>
2018-08-15 10:56:32 +0200

    Add lxml as a requirement in the setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     author_email='florian@festi.info',
     url='https://github.com/florianfesti/boxes',
     packages=find_packages(),
-    install_requires=['cairocffi==0.8.0', 'markdown'],
+    install_requires=['cairocffi==0.8.0', 'markdown', 'lxml'],
     scripts=['scripts/boxes', 'scripts/boxesserver'],
     cmdclass={
         'build_py': CustomBuildExtCommand,

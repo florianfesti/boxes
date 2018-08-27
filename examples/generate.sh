7d356f22673e2f0baa2ae01866ab8603eb67abd7
David Bennett <pinkfishfrog@gmail.com>
2018-08-27 08:58:02 -0700

    Update the generate script to work correctly.

--- examples/generate.sh
+++ examples/generate.sh
@@ -2,20 +2,21 @@
 
 BOXES=../scripts/boxes
 
+set -x
 $BOXES box --x=50 --y=50 --h=70 --output=box.svg
-$BOXES box2 --x=50 --y=50 --h=70 --output=box2.svg
-$BOXES box3 --x=50 --y=50 --h=70 --output=box3.svg
+$BOXES closedbox --x=50 --y=50 --h=70 --output=closedbox.svg
+$BOXES hingebox --x=50 --y=50 --h=70 --output=hingebox.svg
 $BOXES castle --output=castle.svg
 $BOXES drillbox --output=drillbox.svg
 $BOXES flexbox --x=70 --y=100 --h=50 --radius=20 --output=flexbox.svg
 $BOXES flexbox2 --x=70 --y=100 --h=50 --radius=20 --output=flexbox2.svg
 $BOXES flexbox3 --x=70 --y=100 --z=50 --h=8 --radius=30 --output=flexbox3.svg
 $BOXES folder --x=165 --y=240 --h=20 --r=10 --output=folder.svg
-$BOXES lamp --output=lamp.svg
-$BOXES magazinefile --output=magazinefile.svg
+#$BOXES lamp --x=50 --y=50  --r=10 --output=lamp.svg
+$BOXES magazinfile --output=magazinefile.svg
 #$BOXES printer --output=printer.svg
-$BOXES silverwarebox --output=silverwarebox.svg
+#$BOXES Silverwaree --output=silverwarebox.svg
 #$BOXES traylayout --x=4 --y=4 --output=traylayout.txt
-$BOXES traylayout --input=traylayout.txt --h=50 --hi=40 --output=traylayout.svg
+$BOXES traylayout2 --input=traylayout.txt --h=50 --hi=40 --output=traylayout.svg
 $BOXES trayinsert --sx=70:100:70 --sy=100*3 --h=50 --output=trayinsert.svg
 $BOXES typetray --sx=70:100:70 --sy=100*3 --h=60 --hi=50 --output=typetray.svg

8327b272c0792e23d0c79a30f31d88b0867c7a54
Rotzbua <Rotzbua@users.noreply.github.com>
2018-10-03 06:11:42 +0200

    create travis build script for doc

--- documentation/src/conf.py
+++ documentation/src/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

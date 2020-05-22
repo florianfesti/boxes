07e53868f657f14268ceba55eb3caba9f0ad7ac5
Florian Festi <florian@festi.info>
2020-05-22 13:32:43 +0200

    Remove duplicated "f" option in top_edge parameter
    Thanks to Zhong for pointing this out!

--- boxes/__init__.py
+++ boxes/__init__.py
@@ -402,7 +402,7 @@ class Boxes:
                 if default is None: default = "e"
                 self.argparser.add_argument(
                     "--top_edge", action="store",
-                    type=ArgparseEdgeType("efFcESikvfLt"), choices=list("efFcESikvfLt"),
+                    type=ArgparseEdgeType("efFcESikvLt"), choices=list("efFcESikvfLt"),
                     default=default, help="edge type for top edge")
             elif arg == "outside":
                 if default is None: default = True

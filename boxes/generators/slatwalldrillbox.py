216cac23e413b7c2ff60719e702f88a2893f4352
zzj <29055749+zjzh@users.noreply.github.com>
2022-01-20 20:37:29 +0800

    Update slatwalldrillbox.py
    refactoring code with var unpack which is more pythonic, concise, readable and efficient

--- boxes/generators/slatwalldrillbox.py
+++ boxes/generators/slatwalldrillbox.py
@@ -138,11 +138,11 @@ class SlatwallDrillBox(Boxes):
 
         edges[0](x*fa)
         self.corner(a)
-        self.edgeCorner(edges[0], edges[1], 90)
+        self.edgeCorner(*edges[0:2:1], 90)
         edges[1](eh+h)
-        self.edgeCorner(edges[1], edges[2], 90)
+        self.edgeCorner(*edges[1:3:1], 90)
         edges[2](x)
-        self.edgeCorner(edges[2], edges[3], 90)
+        self.edgeCorner(*edges[2:4:1], 90)
         edges[3](h)
         self.edgeCorner(edges[3], edges[0], 90)
 

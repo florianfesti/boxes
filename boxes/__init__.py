50072a18b754e8d2759a05c7e8c54a03c4d06582
Florian Festi <florian@festi.info>
2022-05-08 12:18:15 +0200

    Add stackable edge with finger holes to top_edge
    standard parameter and support it in drawLid()

--- boxes/__init__.py
+++ boxes/__init__.py
@@ -444,7 +444,7 @@ class Boxes:
                 if default is None: default = "e"
                 self.argparser.add_argument(
                     "--top_edge", action="store",
-                    type=ArgparseEdgeType("efFhcESikvLt"), choices=list("efFhcESikvfLt"),
+                    type=ArgparseEdgeType("efFhcESŠikvLt"), choices=list("efFhcESŠikvfLt"),
                     default=default, help="edge type for top edge")
             elif arg == "outside":
                 if default is None: default = True
--- boxes/lids.py
+++ boxes/lids.py
@@ -125,7 +125,7 @@ class _TopEdge(Boxes):
             self.rectangularWall(x, y, "CCCC", bedBolts=[d2, d3, d2, d3], move="up")
         elif top_edge == "f":
             self.rectangularWall(x, y, "FFFF", move="up")
-        elif top_edge == "F" or top_edge == "h":
+        elif top_edge in "FhŠ":
             self.rectangularWall(x, y, "ffff", move="up")
         elif top_edge == "L":
             self.rectangularWall(x, y, "nlmE", move="up")

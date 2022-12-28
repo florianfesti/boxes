6ce8bf73a628a40b8b61dfba5c542cc831e37e59
Rotzbua <Rotzbua@users.noreply.github.com>
2022-12-28 18:14:57 +0100

    Remove Python 2.7 coding hint

--- boxes/edges.py
+++ boxes/edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # Copyright (C) 2013-2016 Florian Festi
 #
 #   This program is free software: you can redistribute it and/or modify
--- boxes/generators/discrack.py
+++ boxes/generators/discrack.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# coding: utf-8
 # Copyright (C) 2019 chrysn <chrysn@fsfe.org>
 #
 #   This program is free software: you can redistribute it and/or modify

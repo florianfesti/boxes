florianfesti/boxes

commit 940cec1811823b1e0a537675171f328e9c1b70ef

polygonWall: Fix numbers of callbacks
They were off by a factor of two as they should be numberd by sides
while we also counted corners

boxes/__init__.py
@@ -2700,7 +2700,7 @@ class Boxes:
 
         length_correction = 0.
         for i in range(0, len(borders), 2):
-            self.cc(callback, i)
+            self.cc(callback, i / 2)
             self.edge(length_correction)
             l = borders[i] - length_correction
             next_angle = borders[i+1]

commit 3e06ef3921ee85037e803b077efb78489e0c6ade

polygonWall: Make sure callback indexes are int
Was broken by 940cec1811823b1e0a537675171f328e9c1b70ef

Resolves HeartBox failing

Thanks to noel hanback <https://hackaday.io/hacker/1310423-noel-hanback>
for reporting.

boxes/__init__.py
@@ -2700,7 +2700,7 @@ class Boxes:
 
         length_correction = 0.
         for i in range(0, len(borders), 2):
-            self.cc(callback, i / 2)
+            self.cc(callback, i // 2)
             self.edge(length_correction)
             l = borders[i] - length_correction
             next_angle = borders[i+1]
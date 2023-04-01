florianfesti/boxes

commit c4b0971d039e718a970037d45372bf82b48e538a

topEdges: Give better names to variables

boxes/lids.py
@@ -107,44 +107,44 @@ class _TopEdge(Boxes):
         """Return top edges belonging to given main edge type
         as a list containing edge for left, back, right, front.
         """
-        t1 = t2 = t3 = t4 = self.edges.get(top_edge, self.edges["e"])
+        tl = tb = tr = tf = self.edges.get(top_edge, self.edges["e"])
 
-        if t1.char == "i":
-            t2 = t4 = "e"
-            t3 = "j"
-        elif t1.char == "k":
-            t2 = t4 = "e"
-        elif t1.char == "L":
-            t1 = "M"
-            t2 = "e"
-            t3 = "N"
-        elif t1.char == "v":
-            t2 = t3 = t4 = "e"
-        elif t1.char == "t":
-            t1 = t3 = "e"
-        elif t1.char == "G":
-            t1 = t2 = t3 = t4 = "e"
+        if tl.char == "i":
+            tb = tf = "e"
+            tr = "j"
+        elif tl.char == "k":
+            tb = tf = "e"
+        elif tl.char == "L":
+            tl = "M"
+            tb = "e"
+            tr = "N"
+        elif tl.char == "v":
+            tb = tr = tf = "e"
+        elif tl.char == "t":
+            tl = tr = "e"
+        elif tl.char == "G":
+            tl = tb = tr = tf = "e"
             if self.edges["G"].settings.side == edges.MountingSettings.PARAM_LEFT:
-                t1 = "G"
+                tl = "G"
             elif self.edges["G"].settings.side == edges.MountingSettings.PARAM_RIGHT:
-                t3 = "G"
+                tr = "G"
             elif self.edges["G"].settings.side == edges.MountingSettings.PARAM_FRONT:
-                t4 = "G"
+                tf = "G"
             else: #PARAM_BACK
-                t2 = "G"
-        elif t1.char == "y":
-            t1 = t2 = t3 = t4 = "e"
+                tb = "G"
+        elif tl.char == "y":
+            tl = tb = tr = tf = "e"
             if self.edges["y"].settings.on_sides == True:
-                t1 = t3 = "y"
+                tl = tr = "y"
             else:
-                t2 = t4 = "y"
-        elif t1.char == "Y":
-            t1 = t2 = t3 = t4 = "h"
+                tb = tf = "y"
+        elif tl.char == "Y":
+            tl = tb = tr = tf = "h"
             if self.edges["Y"].settings.on_sides == True:
-                t1 = t3 = "Y"
+                tl = tr = "Y"
             else:
-                t2 = t4 = "Y"
-        return [t1, t2, t3, t4]
+                tb = tf = "Y"
+        return [tl, tb, tr, tf]
 
     def drawLid(self, x, y, top_edge, bedBolts=[None, None]):
         d2, d3 = bedBolts

commit 51f42fd0a7f2605e1252d1c2a3e015ccaff9699b

Fix top edges
probably broken by c4b0971d039e718a970037d45372bf82b48e538a

Related: #469

boxes/lids.py
@@ -111,17 +111,17 @@ class _TopEdge(Boxes):
 
         if tl.char == "i":
             tb = tf = "e"
-            tr = "j"
+            tl = "j"
         elif tl.char == "k":
-            tb = tf = "e"
+            tl = tr = "e"
         elif tl.char == "L":
             tl = "M"
-            tb = "e"
+            tf = "e"
             tr = "N"
         elif tl.char == "v":
-            tb = tr = tf = "e"
+            tl = tr = tf = "e"
         elif tl.char == "t":
-            tl = tr = "e"
+            tf = tb = "e"
         elif tl.char == "G":
             tl = tb = tr = tf = "e"
             if self.edges["G"].settings.side == edges.MountingSettings.PARAM_LEFT:
@@ -155,16 +155,16 @@ class _TopEdge(Boxes):
         elif top_edge in "FhŠY":
             self.rectangularWall(x, y, "ffff", move="up", label="top")
         elif top_edge == "L":
-            self.rectangularWall(x, y, "nlmE", move="up", label="lid top")
+            self.rectangularWall(x, y, "Enlm", move="up", label="lid top")
         elif top_edge == "i":
-            self.rectangularWall(x, y, "IEJe", move="up", label="lid top")
+            self.rectangularWall(x, y, "EJeI", move="up", label="lid top")
         elif top_edge == "k":
             outset =  self.edges["k"].settings.outset
             self.edges["k"].settings.setValues(self.thickness, outset=True)
             lx = x/2.0-0.1*self.thickness
             self.edges['k'].settings.setValues(self.thickness, grip_length=5)
             self.rectangularWall(lx, y, "IeJe", move="right", label="lid top left")
-            self.rectangularWall(lx, y, "IeJe", move="up", label="lid top right")
+            self.rectangularWall(lx, y, "IeJe", move="mirror up", label="lid top right")
             self.rectangularWall(lx, y, "IeJe", move="left only", label="invisible")
             self.edges["k"].settings.setValues(self.thickness, outset=outset)
         elif top_edge == "S":
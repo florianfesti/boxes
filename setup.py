01c432ec1de56e3229aeced273401e34c4a97603
Florian Festi <florian@festi.info>
2024-02-16 11:29:53 +0100

    Accept later qrcode modules - take 2

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     url='https://github.com/florianfesti/boxes',
     packages=find_packages(),
     python_requires='>=3.8',
-    install_requires=['affine>=2.0', 'markdown', 'shapely>=1.8.2', 'qrcode==7.3.1'],
+    install_requires=['affine>=2.0', 'markdown', 'shapely>=1.8.2', 'qrcode>=7.3.1'],
     scripts=['scripts/boxes', 'scripts/boxesserver'],
     cmdclass={
         'build_py': CustomBuildExtCommand,

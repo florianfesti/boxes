d8b5e45b61587d47054e8680b4dd814b79fc0304
Florian Festi <florian@festi.info>
2024-12-27 15:32:33 +0100

    Make gen_thumbnails.sh more robust
    Also use magick instead of convert whihc is deprecated.

--- scripts/gen_thumbnails.sh
+++ scripts/gen_thumbnails.sh
@@ -8,7 +8,7 @@ THUMB_HEIGHT=10000 # height: auto;
 
 thumbnail() {
 	echo "convert \"$1\" -thumbnail ${THUMB_WIDTH}x${THUMB_HEIGHT} \"${1%.*}-thumb.jpg\""
-	convert "$1" -thumbnail ${THUMB_WIDTH}x${THUMB_HEIGHT} "${1%.*}-thumb.jpg"
+	magick "$1" -thumbnail ${THUMB_WIDTH}x${THUMB_HEIGHT} "${1%.*}-thumb.jpg"
 	git add "$1" "${1%.*}-thumb.jpg" "$STATIC_DIR"samples.sha256
 }
 
@@ -16,13 +16,13 @@ thumbnail() {
 
 find "$STATIC_DIR" -name '*.jpg' ! -name '*-thumb.jpg' -type f | while read -r f
 do
-	f_=$(echo "$f" | sed -E -e 's@([.])@\\\1@g')
+        f_=$(echo "$f" | sed -E -e 's@([.])@\\\1@g')
 	checksum=$(grep "$f_" "$STATIC_DIR"samples.sha256 || true)
 	if [ -n "$checksum" ]
 	then
 		echo "$checksum" | sha256sum -c --status || {
 			echo "File $f changed"
-			sed -i "/$f_/ c\\
+			sed -i "\\@$f_@ c\\
 $(sha256sum "$f")
 " "$STATIC_DIR"samples.sha256
 			thumbnail "$f"

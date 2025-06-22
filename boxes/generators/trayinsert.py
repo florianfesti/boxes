745db813fbde39761fad4ccebc0047919e1bcd23
Florian Festi <florian@festi.info>
2025-06-22 20:01:14 +0200

    TrayInsert: Add draft_angle parameter
    Allow fitting into injection molded boxes. These often have a draft angle
    with the walls being slightly angle outward towards the top.

--- boxes/generators/trayinsert.py
+++ boxes/generators/trayinsert.py
@@ -15,6 +15,22 @@
 
 from boxes import *
 
+class SlantedEdge(edges.BaseEdge):
+
+    def __call__(self, length, **kw):
+        angle = abs(self.settings)
+        if angle == 0:
+            poly = [length]
+        else:
+            d = length * math.tan(math.radians(angle))
+            l = length / math.cos(math.radians(angle))
+            poly = [0, -90, d, 90+angle, l, -angle, 0]
+
+        if self.settings >= 0:
+            poly.reverse()
+
+        self.polyline(*poly)
+
 class TrayInsert(Boxes):
     """Tray insert without floor and outer walls - allows only continuous walls"""
 
@@ -54,8 +70,10 @@ This frees your mind into separately thinking about the cell size vs the overall
             help="X dimension of tray/box that this fits into")
         self.argparser.add_argument(
             "--y", action="store", type=float, default=-1,
-            help="Y dimension of tray/box that this fits into.  ")
-
+            help="Y dimension of tray/box that this fits into")
+        self.argparser.add_argument(
+            "--draft_angle", action="store", type=float, default=0.0,
+            help="amount the walls angle outwards toward to top (above sizes are at the bottom)")
 
     def render(self):
 
@@ -67,6 +85,9 @@ This frees your mind into separately thinking about the cell size vs the overall
         x = sum(self.sx) + t * (len(self.sx) - 1)
         y = sum(self.sy) + t * (len(self.sy) - 1)
         h = self.h
+
+        l = SlantedEdge(self, -self.draft_angle)
+        r = SlantedEdge(self, self.draft_angle)
 
         if self.x > x:
             delta = self.x-x
@@ -94,9 +115,9 @@ This frees your mind into separately thinking about the cell size vs the overall
 
         # Inner walls
         for i in range(len(self.sx) - 1):
-            e = [edges.SlottedEdge(self, self.sy, slots=0.5 * h), "e", "e", "e"]
+            e = [edges.SlottedEdge(self, self.sy, slots=0.5 * h), r, "e", l]
             self.rectangularWall(y, h, e, move="up")
 
         for i in range(len(self.sy) - 1):
-            e = ["e", "e", edges.SlottedEdge(self, self.sx[::-1], "e", slots=0.5 * h), "e"]
+            e = ["e", r, edges.SlottedEdge(self, self.sx[::-1], "e", slots=0.5 * h), l]
             self.rectangularWall(x, h, e, move="up")

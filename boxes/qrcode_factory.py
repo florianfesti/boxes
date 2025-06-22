8b8195779deba79b5ea2eec70e3457292bb391cb
Rotzbua <Rotzbua@users.noreply.github.com>
2025-06-22 17:03:24 +0200

    Add typing to QR code factory

--- boxes/qrcode_factory.py
+++ boxes/qrcode_factory.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 from decimal import Decimal
+from typing import Any, IO
 
 import qrcode.image.base
 import qrcode.image.svg
@@ -13,51 +16,52 @@ class BoxesQrCodeFactory(qrcode.image.base.BaseImage):
     kind = "SVG"
     allowed_kinds = ("SVG",)
 
-    def __init__(self, *args, ctx=None, x=0, y=0, **kwargs):
+    def __init__(self, *args, ctx=None, x: float = 0, y: float = 0, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.ctx = ctx
         self.x, self.y = x, y
         # Save the unit size, for example the default box_size of 10 is '1mm'.
         self.unit_size = self.units(self.box_size)
 
-    def drawrect(self, row, col):
+    def drawrect(self, row: float, col: float) -> None:
         self.ctx.rectangle(*self._rect(row, col))
         self._img.append(self._rect(row, col))
 
-    def units(self, pixels, text=True):
+    def units(self, pixels: Any, text: bool = True) -> Decimal | str:
         """
         A box_size of 10 (default) equals 1mm.
         """
         units = Decimal(pixels) / 10
         if not text:
             return units
-        return '%smm' % units
+        return f'{units}mm'
 
-    def save(self, stream, kind=None):
+    def save(self, stream: IO[bytes], kind: str | None = None) -> None:
         self.check_kind(kind=kind)
         self._write(stream)
 
-    def to_string(self):
+    def to_string(self) -> str:
         return f"".join(self._img)
 
-    def new_image(self, **kwargs):
-        self._img = []
+    def new_image(self, **kwargs: Any) -> Any:
+        self._img = []  # type: ignore
         return self._img
 
-    def _rect(self, row, col):
+    def _rect(self, row: float, col: float) -> tuple[float, float, float, float]:
         size = self.box_size / 10
         x = self.x + (row + self.border) * size
         y = self.y + (col + self.border) * size
         return x, y, size, size
 
-    def _write(self, stream):
+    def _write(self, stream) -> None:
         stream.write("".join(self._img))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     import qrcode
     import qrcode.image
+
     q = qrcode.QRCode(image_factory=BoxesQrCodeFactory, box_size=10)
-    q.add_data('hello')
-    ctx = "a context"
+    q.add_data("hello")
     img = q.make_image(ctx="a context")
     print(img.to_string())

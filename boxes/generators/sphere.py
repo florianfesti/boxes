306f4cc0c775caabfc841c041b869b846870dfff
Jolien <jolienfranke@gmail.com>
2025-06-28 22:12:36 +0200

    Added cable hook feature and refactored render function

--- boxes/generators/sphere.py
+++ boxes/generators/sphere.py
@@ -45,6 +45,12 @@ class Sphere(Boxes):
         self.argparser.add_argument(
             "--scoring_lines", action="store", type=boolarg, default=False,
             help="Add scoring lines to easier fold tabs")
+        self.argparser.add_argument(
+            "--cable_hook", action="store", type=int, default=0,
+            help="A hook to hang the sphere on a cable (for e.g. lamps), the amount of gores that have a hook")
+        self.argparser.add_argument(
+            "--cable_hook_radius", action="store", type=float, default=3,
+            help="The radius of the cable for the cable hook")
 
         defaultgroup = self.argparser._action_groups[1]
         for action in defaultgroup._actions:
@@ -155,6 +161,29 @@ class Sphere(Boxes):
 
         return points
 
+    def coordinatesCableHook(self):
+        skewfactor = 0.8
+        topWidth = 2 * self.x_rightGoreTop
+        delta = (topWidth * (1 - skewfactor)) / 2
+        outercurveRadius = (topWidth * skewfactor) / 2
+        tipRadius = (outercurveRadius * 2 - self.cable_hook_radius * 2) / 4
+
+        points = []
+        points.append((self.x_rightGoreTop - delta, self.u_goreTop + topWidth))
+        points.append(self.TurnToDirection(90))
+        points.append(self.Curve(180, outercurveRadius))
+        points.append(0)
+        points.append(self.Curve(180, tipRadius))
+        points.append(0)
+        points.append(self.Curve(-180, self.cable_hook_radius))
+        points.append(0)
+        points.append(self.Curve(-90, outercurveRadius))
+        points.append(0)
+        points.append(((-self.x_rightGoreTop + delta + (tipRadius * 2 + (self.cable_hook_radius * 2) - outercurveRadius)),
+                       self.u_goreTop + topWidth - outercurveRadius))
+
+        return points
+
     def coordinatesToPolyline(self, points):
         polyPoints = [0]
         distance = 0
@@ -201,9 +230,6 @@ class Sphere(Boxes):
     def coordinatesTabStart(self):
         return [self.Curve(-180, self.thickness / 2), 0, self.Curve(180, (self.tab_width - self.thickness) / 2), 0]
 
-    def coordinatesTabStart(self):
-        return [self.Curve(-180, self.thickness / 2), 0, self.Curve(180, (self.tab_width - self.thickness) / 2), 0]
-
     def coordinatesTabEnd(self):
         return [self.Curve(180, (self.tab_width - self.thickness) / 2), 0, self.Curve(-180, self.thickness / 2), 0]
 
@@ -213,7 +239,69 @@ class Sphere(Boxes):
                               self.u_goreTop - tinyTabLength,
                               num=numberOfTabs + 1)
 
+    def assemble(self, hook=False):
+        coordinates = []
+        coordinates.extend(self.coordinatesPartOfGore(self.u_goreBottom, self.u_tabPoints[0], True, True))
+
+        for i in range(0, len(self.u_tabPoints) - 1, 2):
+            coordinates.extend(self.coordinatesTabStart())
+            coordinates.extend(self.coordinatesPartOfOffset(self.u_tabPoints[i], self.u_tabPoints[i + 1], self.tab_width, True, True))
+            coordinates.extend(self.coordinatesTabEnd())
+            coordinates.extend(self.coordinatesPartOfGore(self.u_tabPoints[i + 1], self.u_tabPoints[i + 2], True, True))
+
+        lastTabPoint = self.u_tabPoints[-1]
+        upperCornerU = lastTabPoint + self.normalCompensation(lastTabPoint)
+
+        coordinates.extend(self.coordinatesTabStart())
+
+        if (upperCornerU < self.u_goreTop):
+            coordinates.extend(self.coordinatesPartOfOffset(lastTabPoint, self.u_goreTop - self.normalCompensation(self.u_goreTop), self.tab_width, True)) # line is not entirely straight, because te normalcompensation is simplified (should be at a bit lower u)
+        else:
+            coordinates.extend([(self.calculateXofOffsetGore(lastTabPoint), upperCornerU)])
+        coordinates.extend([(self.x_rightGoreTop, self.u_goreTop)])
+
+        if (hook):
+            coordinates.extend(self.coordinatesCableHook())
+        else:
+            coordinates.extend(self.coordinatesTopHole(self.x_rightGoreTop, -self.x_rightGoreTop))
+
+        #Left half
+        coordinates.extend(self.coordinatesPartOfGore(self.u_goreTop, lastTabPoint, False))
+
+        for i in range(len(self.u_tabPoints) - 1, 0, -2):
+            coordinates.extend(self.coordinatesTabStart())
+            coordinates.extend(self.coordinatesPartOfOffset(self.u_tabPoints[i], self.u_tabPoints[i - 1], self.tab_width, False))
+            coordinates.extend(self.coordinatesTabEnd())
+            coordinates.extend(self.coordinatesPartOfGore(self.u_tabPoints[i - 1], self.u_tabPoints[i - 2], False))
+
+        firstTabPoint = self.u_tabPoints[0]
+        lowerCornerU = firstTabPoint + self.normalCompensation(firstTabPoint)
+
+        coordinates.extend(self.coordinatesTabStart())
+
+        if (lowerCornerU > self.u_goreBottom):
+            coordinates.extend(self.coordinatesPartOfOffset(firstTabPoint, self.u_goreBottom - self.normalCompensation(self.u_goreBottom), self.tab_width, False))
+        else:
+            coordinates.extend([(-self.calculateXofOffsetGore(firstTabPoint), lowerCornerU)])
+        coordinates.extend([(-self.x_rightGoreBottom, self.u_goreBottom)])
+
+        coordinates.extend([(self.x_rightGoreBottom, self.u_goreBottom)])
+
+        return coordinates
+
+
     def render(self):
+        self.resolution = int(self.sphere_radius / 10)  # This is arbitrary. I just want the resolution to be proportional to the total size
+        self.gore_heigth = math.pi * self.sphere_radius  # the midline of the gore is the same as the line on the sphere, so half a circumference
+        self.bellyLens = 2 * math.tan((2 * math.pi) / (2 * self.amount_gores)) * self.sphere_radius
+        self.halfBellyLens = self.bellyLens / 2
+
+        self.x_rightGoreTop = self.calculateXofTopAndGoreIntersection()
+        self.u_goreTop = self.calculateUpperUOfGore(self.x_rightGoreTop)
+        self.u_goreBottom = self.calculateUOfBottomHole()
+        self.x_rightGoreBottom = self.calculateXOfGore(self.u_goreBottom)
+        self.u_tabPoints = self.divideGore(self.tabs, self.corner_tab)
+
         if self.tabs % 2 == 1:
             raise ValueError("The number of tabs has to be even")
         if self.tab_width <= self.thickness:
@@ -230,70 +318,22 @@ class Sphere(Boxes):
             raise ValueError("The sphere radius cannot be negative")
         if self.thickness / 2 < self.burn:
             raise ValueError("The material thickness has to be at least twice the burn thickness")
-
-        allCoordinates = []
-
-        self.resolution = int(self.sphere_radius / 10)  # This is arbitrary. I just want the resolution to be proportional to the total size
-        self.gore_heigth = math.pi * self.sphere_radius  # the midline of the gore is the same as the line on the sphere, so half a circumference
-        self.bellyLens = 2 * math.tan((2 * math.pi) / (2 * self.amount_gores)) * self.sphere_radius
-        self.halfBellyLens = self.bellyLens / 2
-
-        self.x_rightGoreTop = self.calculateXofTopAndGoreIntersection()
-        self.u_goreTop = self.calculateUpperUOfGore(self.x_rightGoreTop)
-        self.u_goreBottom = self.calculateUOfBottomHole()
-        self.x_rightGoreBottom = self.calculateXOfGore(self.u_goreBottom)
-        self.u_tabPoints = self.divideGore(self.tabs, self.corner_tab)
-
-        # Right half
-        allCoordinates.extend(self.coordinatesPartOfGore(self.u_goreBottom, self.u_tabPoints[0], True, True))
-
-        for i in range(0, len(self.u_tabPoints) - 1, 2):
-            allCoordinates.extend(self.coordinatesTabStart())
-            allCoordinates.extend(self.coordinatesPartOfOffset(self.u_tabPoints[i], self.u_tabPoints[i + 1], self.tab_width, True, True))
-            allCoordinates.extend(self.coordinatesTabEnd())
-            allCoordinates.extend(self.coordinatesPartOfGore(self.u_tabPoints[i + 1], self.u_tabPoints[i + 2], True, True))
-
-        lastTabPoint = self.u_tabPoints[-1]
-        upperCornerU = lastTabPoint + self.normalCompensation(lastTabPoint)
-
-        allCoordinates.extend(self.coordinatesTabStart())
-
-        if (upperCornerU < self.u_goreTop):
-            allCoordinates.extend(self.coordinatesPartOfOffset(lastTabPoint, self.u_goreTop - self.normalCompensation(self.u_goreTop), self.tab_width, True)) # line is not entirely straight, because te normalcompensation is simplified (should be at a bit lower u)
-        else:
-            allCoordinates.extend([(self.calculateXofOffsetGore(lastTabPoint), upperCornerU)])
-        allCoordinates.extend([(self.x_rightGoreTop, self.u_goreTop)])
-
-        allCoordinates.extend(self.coordinatesTopHole(self.x_rightGoreTop, -self.x_rightGoreTop))
-
-        #Left half
-        allCoordinates.extend(self.coordinatesPartOfGore(self.u_goreTop, lastTabPoint, False))
-
-        for i in range(len(self.u_tabPoints) - 1, 0, -2):
-            allCoordinates.extend(self.coordinatesTabStart())
-            allCoordinates.extend(self.coordinatesPartOfOffset(self.u_tabPoints[i], self.u_tabPoints[i - 1], self.tab_width, False))
-            allCoordinates.extend(self.coordinatesTabEnd())
-            allCoordinates.extend(self.coordinatesPartOfGore(self.u_tabPoints[i - 1], self.u_tabPoints[i - 2], False))
-
-        firstTabPoint = self.u_tabPoints[0]
-        lowerCornerU = firstTabPoint + self.normalCompensation(firstTabPoint)
-
-        allCoordinates.extend(self.coordinatesTabStart())
-
-        if (lowerCornerU > self.u_goreBottom):
-            allCoordinates.extend(self.coordinatesPartOfOffset(firstTabPoint, self.u_goreBottom - self.normalCompensation(self.u_goreBottom), self.tab_width, False))
-        else:
-            allCoordinates.extend([(-self.calculateXofOffsetGore(firstTabPoint), lowerCornerU)])
-        allCoordinates.extend([(-self.x_rightGoreBottom, self.u_goreBottom)])
-
-        allCoordinates.extend([(self.x_rightGoreBottom, self.u_goreBottom)])
-
-        polyPoints = self.coordinatesToPolyline(allCoordinates)
+        if self.cable_hook_radius + 1 > 0.8 * self.x_rightGoreTop:
+            raise ValueError("The cable hook radius is too big for this size top hole")
+        if self.cable_hook > self.amount_gores:
+            raise ValueError("The amount of hooks cannot be larger than the amount of gores")
 
         self.moveTo(-self.halfBellyLens, 30)
 
         moveX = self.bellyLens + self.tab_width * 2 + 30
+
+        drawHook = self.cable_hook
         for i in range(self.amount_gores):
+
+            if (drawHook > 0):
+                polyPoints = self.coordinatesToPolyline(self.assemble(True))
+            else:
+                polyPoints = self.coordinatesToPolyline(self.assemble())
 
             self.ctx.save()
             self.moveTo(0, -self.burn)
@@ -306,10 +346,12 @@ class Sphere(Boxes):
 
                 for i in range(0, len(self.u_tabPoints) - 1, 2):
                     self.scoringLines(self.u_tabPoints[i], self.u_tabPoints[i + 1], True)
-                self.scoringLines(lastTabPoint, self.u_goreTop, True)
+                self.scoringLines(self.u_tabPoints[-1], self.u_goreTop, True)
                 for i in range(len(self.u_tabPoints) - 1, 0, -2):
                     self.scoringLines(self.u_tabPoints[i], self.u_tabPoints[i - 1], False)
-                self.scoringLines(firstTabPoint, self.u_goreBottom, False)
+                self.scoringLines(self.u_tabPoints[0], self.u_goreBottom, False)
                 self.ctx.restore()
+
+            drawHook -= 1
 
             self.moveTo(moveX, 0)

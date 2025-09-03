ecd411bb47628e58c654678c3360d2428690f2b0
Richard Bowman <richard.bowman@cantab.net>
2025-09-03 13:11:23 +0100

    Added an option for a latched lid to RoundedBox
    This adds an additional value for `--top` which is `lid_with_latches`. Using this will add an extra plate that
    sits just inside (i.e. underneath) the lid, with four latches on it.
    The latches may be moved in and out using four M3 screws.
    
    The lid must be glued together with two smaller plates, to make a single unit. Also, the holes for the screws need to be threaded before they will work - depending on the material it may be possible to self-tap.
    
    I've printed this in 3mm acrylic and it worked fairly well.

--- boxes/generators/roundedbox.py
+++ boxes/generators/roundedbox.py
@@ -14,6 +14,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import boxes
+from math import sqrt
 
 
 class RoundedBox(boxes.Boxes):
@@ -50,16 +51,21 @@ With lid:
             help="edge type for top and bottom edges")
         self.argparser.add_argument(
             "--top",  action="store", type=str, default="hole",
-            choices=["hole", "lid", "closed",],
+            choices=["hole", "lid", "closed", "lid_with_latches"],
             help="style of the top and lid")
 
-    def hole(self):
+    @property
+    def hole_dr(self):
+        dr = 2*self.thickness
+        if self.edge_style == "h":
+            dr = self.thickness
+        return dr
+
+    def top_hole(self):
         t = self.thickness
         x, y, r = self.x, self.y, self.radius
 
-        dr = 2*t
-        if self.edge_style == "h":
-            dr = t
+        dr = self.hole_dr
 
         if r > dr:
             r -= dr
@@ -74,6 +80,73 @@ With lid:
         for l in (lx, ly, lx, ly):
             self.edge(l)
             self.corner(90, r)
+
+        if self.top == "lid_with_latches":
+            # We end up where we started, so move up further to the
+            # centre of the radiused corner
+            self.moveTo(0, r)  # Note r=self.radius-dr
+            self.screw_slots(lx, ly, 3.3/2)
+
+    @boxes.holeCol
+    def screw_slots(self, lx, ly, r):
+        """Make four slots with rounded ends, at the four corners.
+
+        These slots allow screws to slide along them, to move the latches
+        in and out.
+        """
+        for l in (lx, ly, lx, ly):
+            self.moveTo(0, 0, 45)
+            self.moveTo(0, -r)
+            self.corner(180, r)
+            self.edge(2*self.thickness)
+            self.corner(180, r)
+            self.edge(2*self.thickness)
+            self.moveTo(0, r, -45)
+            self.moveTo(l, 0, 90)
+
+    def screw_clearance_slots(self):
+        t = self.thickness
+        x, y, r = self.x, self.y, self.radius
+
+        self.moveTo(0, r + t)
+        self.screw_slots(x - 2*r, y - 2*r, 5)
+
+    def latches(self):
+        """Screw holes for tapping on latches"""
+        t = self.thickness
+        x, y, r = self.x - 4*t, self.y - 4*t, self.radius - 2*t
+
+        # we end up at(r, 0), where the curve joins the edge
+        for l in [y-2*r, x-2*r, y-2*r, x-2*r]:
+            self.hole(0, r, d=2.5)
+            # This will cut a finger out of the corner
+            self.moveTo(0, 0, 45)
+            self.edge(40)
+            self.corner(180, r/sqrt(2))
+            self.edge(40)
+            self.moveTo(0, 0, -135)
+            self.moveTo(l, 0, 180)
+
+    def latch_screw_slots(self):
+        """Slots for the screws to slide in"""
+        t = self.thickness
+        # This will be on the "top" plate with the hole, so it's
+        # actually bigger than nominal by t in all directions.
+        x, y, r = self.x + 2*t, self.y + 2*t, self.radius + t
+        screw_r = 3.3/2
+
+        # we end up at(r, 0), where the curve joins the edge
+        for l in [y-2*r, x-2*r, y-2*r, x-2*r]:
+            # This will cut a slot, and move from one end of the
+            # curve to the other
+            self.moveTo(0, r, 45)
+            self.moveTo(-2*t, -screw_r)
+            self.edge(2*t)
+            self.corner(180, screw_r)
+            self.edge(2*t)
+            self.corner(180, screw_r)
+            self.moveTo(0, 0, -45)
+            self.moveTo(l, 0, 90)
 
     def cb(self, nr):
         h = 0.5 * self.thickness
@@ -112,23 +185,39 @@ With lid:
             ec = True
 
         with self.saved_context():
+            # This plate is the base of the box
             self.roundedPlate(x, y, r, es, wallpieces=self.wallpieces,
                               extend_corners=ec, move="right")
+            # These are the inner shelves
             for dh in self.sh[:-1]:
                 self.roundedPlate(x, y, r, "f", wallpieces=self.wallpieces,
                                   extend_corners=False, move="right")
+            # This is the top plate (which has a hole in it, if requested)
             self.roundedPlate(x, y, r, es, wallpieces=self.wallpieces,
                               extend_corners=ec, move="right",
-                              callback=[self.hole] if self.top != "closed" else None)
-            if self.top == "lid":
+                              callback=[self.top_hole] if self.top != "closed" else None)
+            # An additional plate for the lid, if requested
+            if self.top in ["lid", "lid_with_latches"]:
                 r_extra = self.edges[self.edge_style].spacing()
                 self.roundedPlate(x+2*r_extra,
                                   y+2*r_extra,
                                   r+r_extra,
                                   "e", wallpieces=self.wallpieces,
-                                  extend_corners=False, move="right")
+                                  extend_corners=False, move="right",
+                                  callback=[self.screw_clearance_slots] if self.top == "lid_with_latches" else None)
 
+            # A plate with the latches.
+            if self.top == "lid_with_latches":
+                dr = self.hole_dr
+                if r < dr:
+                    raise ValueError("Latches only work with radius > %f." % dr)
+                self.roundedPlate(x - 2*dr, y - 2*dr, r - dr, "e", wallpieces=self.wallpieces,
+                                  extend_corners=False, move="right",
+                                  callback=[self.latches])
+
+        # I don't know what this plate is for!
         self.roundedPlate(x, y, r, es, wallpieces=self.wallpieces, move="up only")
 
+        # This is the wall (i.e. the vertical part)
         self.surroundingWall(x, y, r, h, pe, pe, pieces=self.wallpieces,
                              callback=self.cb)

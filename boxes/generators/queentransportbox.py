5b97a7ebdbbcc9d58eec1d9b41e031c210113b92
Martin Scharrer <martin.scharrer@web.de>
2025-10-20 13:37:58 +0200

    Add cutout functionality for QueenTransportBox with QueenCageCutout

--- boxes/generators/queentransportbox.py
+++ boxes/generators/queentransportbox.py
@@ -17,6 +17,89 @@ from boxes import *
 from boxes.generators.drillbox import DrillBox
 from boxes.lids import LidSettings
 
+class Cutout:
+    DIMENSIONS = (0.0, 0.0)
+
+    @staticmethod
+    def transform_point(u, v, tx, ty):
+        return (tx - u), (ty - v)
+
+    def cutout(self, box, x, y, layer=0, color=Color.INNER_CUT):
+        pass
+
+
+class PolygonCutout(Cutout):
+    PTS = (( (0.0, 0.0), ), )
+
+    def get_points(self, layer):
+        return self.PTS[layer]
+
+    def cutout(self, box, x, y, layer=0, color=Color.INNER_CUT):
+        """
+        Fügt den Käfig-Ausschnitt (aus SVG umgewandelt) in die Platte ein.
+        cx, cy = Mittelpunkt der Aussparung
+        """
+        p = box.ctx
+
+
+        with box.saved_context():
+            box.set_source_color(color)
+            transform_point = self.transform_point
+            pts = self.get_points(layer)
+
+            if pts:
+                # Move to first transformed point, then line_to the rest
+                fx, fy = transform_point(pts[0][0], pts[0][1], x, y)
+                p.move_to(fx, fy)
+                for u, v in pts[1:]:
+                    fx, fy = transform_point(u, v, x, y)
+
+                    p.line_to(fx, fy)
+
+                p.stroke()
+
+
+class SingleLayerPolygonCutout(PolygonCutout):
+
+    def get_points(self, layer):
+        """Always return the first and only layer, except for layer 0 which is empty"""
+        if layer == 0:
+            return tuple()
+        return self.PTS[0]
+
+
+class QueenCageCutout(SingleLayerPolygonCutout):
+    DIMENSIONS = (36.2, 15.5)
+    PTS = ((
+            (-7.425925777777778, -6.722116592592595),
+            (-7.425925777777778, -9.222116592592595),
+            (-10.675925777777778, -9.222116592592595),
+            (-10.675925777777778, -6.722116592592595),
+            (-13.675925777777778, -6.722116592592595),
+            (-16.675925777777778, 3.277883407407405),
+            (-17.175925777777778, 3.277883407407405),
+            (-17.775931777777778, 3.277883407407405),
+            (-17.775931777777778, 6.277883407407405),
+            (-8.925925777777778, 6.277883407407405),
+            (-8.925925777777778, 5.9021704074074055),
+            (-8.425925777777778, 5.9021704074074055),
+            (-7.925925777777778, 6.277883407407405),
+            (8.574074222222222, 6.277883407407405),
+            (8.574074222222222, 5.9021704074074055),
+            (9.074074222222222, 5.9021704074074055),
+            (9.574074222222222, 6.277883407407405),
+            (17.824074222222222, 6.277883407407405),
+            (18.42407822222222, 6.277883407407405),
+            (18.42407822222222, 3.277883407407405),
+            (17.324074222222222, 3.277883407407405),
+            (14.324074222222222, -6.722116592592595),
+            (11.324074222222222, -6.722116592592595),
+            (11.324074222222222, -9.222116592592595),
+            (8.074074222222222, -9.222116592592595),
+            (8.074074222222222, -6.722116592592595),
+            (-7.425925777777778, -6.722116592592595)
+    ),)
+
 
 class QueenTransportBox(DrillBox):
     """A simple Box"""
@@ -24,6 +107,8 @@ class QueenTransportBox(DrillBox):
     description = "Queen Transport Box"
 
     ui_group = "Box"
+
+    CUTOUT_DIMENSIONS = (36.2, 15.5)
 
     def __init__(self) -> None:
         Boxes.__init__(self)
@@ -38,72 +123,20 @@ class QueenTransportBox(DrillBox):
             "--top_edge", action="store",
             type=ArgparseEdgeType("eStG"), choices=list("eStG"),
             default="e", help="edge type for top edge")
-        self.buildArgParser(sx="25*3", sy="30*3", sh="1:90", bottom_edge="s")
+        self.buildArgParser(sx="5:45*3:5", sy="5:25*3:5", sh="25:75", bottom_edge="s")
 
-    def cutouts(self, toplayer=False):
+    def cutouts(self, layer=0):
         y = 0
         d = 1.
+        cutout = QueenCageCutout()
         for dy in self.sy:
             x = 0
             for dx in self.sx:
-                self.cutout(x + dx / 2., y + dy / 2.)
-                if toplayer:
-                    pass  # TODO: add cutout extras for top layer
+                if dx > cutout.DIMENSIONS[0] and dy > cutout.DIMENSIONS[1]:
+                    cutout.cutout(self, x + dx / 2., y + dy / 2., layer)
                 x += dx
             y += dy
 
-    def cutout(self, x, y):
-        """
-        Fügt den Käfig-Ausschnitt (aus SVG umgewandelt) in die Platte ein.
-        cx, cy = Mittelpunkt der Aussparung
-        """
-        p = self.ctx
-
-        p.save()
-        # Der Originalpfad (SVG <path d="...">) in Path-Befehle übersetzt:
-        p.move_to(27.125, 40.305031)
-        p.line_to(29.625, 40.305031)
-        p.line_to(29.625, 43.555031)
-        p.line_to(27.125, 43.555031)
-        p.line_to(27.125, 46.555031)
-        p.line_to(17.125, 49.555031)
-        p.line_to(17.125, 50.055031)
-        p.line_to(17.125, 50.655037)
-        p.line_to(14.125, 50.655037)
-        p.line_to(14.125, 41.805031)
-
-        # Diese Linien wurden im SVG durch C-Kommandos (Cubic Bézier) erzeugt,
-        # aber da sie sehr kurz und linear sind, können sie als line_to() interpretiert werden.
-        p.line_to(14.500713, 41.805031)
-        p.line_to(14.500713, 41.305031)
-        p.line_to(14.125, 40.805031)
-        p.line_to(14.125, 24.305031)
-        p.line_to(14.500713, 24.305031)
-        p.line_to(14.500713, 23.805031)
-        p.line_to(14.125, 23.305031)
-        p.line_to(14.125, 15.055031)
-        p.line_to(14.125, 14.455027)
-        p.line_to(17.125, 14.455027)
-        p.line_to(17.125, 15.555031)
-        p.line_to(27.125, 18.555031)
-        p.line_to(27.125, 21.555031)
-        p.line_to(29.625, 21.555031)
-        p.line_to(29.625, 24.805031)
-        p.line_to(27.125, 24.805031)
-        p.line_to(27.125, 40.305031)
-        # p.close_path()
-
-        # Der ursprüngliche SVG-Pfad war gedreht (transform="matrix(-0,1,-1,-0,...)")
-        # Das entspricht einer 90°-Drehung + Spiegelung. Wir wenden die Transformation an:
-        p.rotate(90)
-        p.scale(1, -1)
-
-        # Jetzt verschieben wir den Pfad so, dass sein Mittelpunkt bei (cx, cy) liegt:
-        p.translate(x, -y)
-
-        # Pfad ausschneiden
-        p.stroke()
-        p.restore()
 
     def render(self):
         x = sum(self.sx)
@@ -116,25 +149,23 @@ class QueenTransportBox(DrillBox):
         self.rectangularWall(
             x, h, [b, "f", t1, "F"],
             ignore_widths=[1, 6],
-            callback=[lambda: self.sideholes(x)], move="right")
+            callback=[lambda: self.sideholes(x)], move="right", label='Front')
         self.rectangularWall(
             y, h, [b, "f", t2, "F"], callback=[lambda: self.sideholes(y)],
             ignore_widths=[1, 6],
-            move="up")
+            move="up", label='Left')
         self.rectangularWall(
             y, h, [b, "f", t3, "F"], callback=[lambda: self.sideholes(y)],
-            ignore_widths=[1, 6])
+            ignore_widths=[1, 6], label='Right')
         self.rectangularWall(
             x, h, [b, "f", t4, "F"],
             ignore_widths=[1, 6],
-            callback=[lambda: self.sideholes(x)], move="left up")
+            callback=[lambda: self.sideholes(x)], move="left up", label='Back')
         if b != "e":
-            self.rectangularWall(x, y, "ffff", move="right")
-        for d in self.sh[:-2]:
             self.rectangularWall(
-                x, y, "ffff", callback=[self.cutouts], move="right")
-        self.rectangularWall(
-            x, y, "ffff",
-            callback=[lambda: self.cutouts(toplayer=True)],
-            move="right")
+                x, y, "ffff", callback=[lambda: self.cutouts(layer=0)], move="up", label=f'Bottom Layer')
+        for layer in range(1, len(self.sh)):
+            self.rectangularWall(
+                x, y, "ffff", callback=[lambda: self.cutouts(layer)], move="up", label=f'Layer {layer}')
         self.lid(x, y, self.top_edge)
+

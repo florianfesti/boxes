03ffebe1465d3160e61a9faaf8cb0a8b00c8a837
Martin Scharrer <martin.scharrer@web.de>
2025-10-22 15:21:24 +0200

    Rename `space` argument to `spacing` to avoid clash with existing generator argument.

--- boxes/__init__.py
+++ boxes/__init__.py
@@ -384,14 +384,14 @@ class Boxes:
         defaultgroup.add_argument(
             "--burn", action="store", type=float, default=0.1,
             help='burn correction (in mm)(bigger values for tighter fit) [\U0001F6C8](https://florianfesti.github.io/boxes/html/usermanual.html#burn)')
-        def space_type(x):
+        def spacing_type(x):
             try:
                 return (float(x), 0.)
             except ValueError:
                 return tuple(float(v.strip()) for v in x.split(":"))
         defaultgroup.add_argument(
-            "--space", action="store", type=space_type, default="0.5",
-            help='space around parts (multiples of thickness [: extra space in mm]) [\U0001F6C8](https://florianfesti.github.io/boxes/html/usermanual.html#spacing)')
+            "--spacing", action="store", type=spacing_type, default="0.5",
+            help='spacing around parts (multiples of thickness [: extra space in mm]) [\U0001F6C8](https://florianfesti.github.io/boxes/html/usermanual.html#spacing)')
 
     @contextmanager
     def saved_context(self):
@@ -440,7 +440,7 @@ class Boxes:
             self.ctx.set_line_width(max(2 * self.burn, 0.05))
             self.set_source_color(Color.BLACK)
 
-        self.spacing = 2 * self.burn + self.space[0] * self.thickness + self.space[1]
+        self.spacing = 2 * self.burn + self.spacing[0] * self.thickness + self.spacing[1]
         self.set_font("sans-serif")
         self._buildObjects()
         if self.reference and self.format != 'svg_Ponoko':

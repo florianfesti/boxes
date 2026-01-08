c4aba081c1e497b613afb23e7c0374bf624deb4a
Florian Festi <florian@festi.info>
2026-01-08 19:42:20 +0100

    UBox: Use x, y, h correctly
    Related: #831

--- boxes/generators/ubox.py
+++ boxes/generators/ubox.py
@@ -33,77 +33,77 @@ class UBox(_TopEdge):
             help="radius of bottom corners")
         self.angle = 0
 
-    def U(self, x, y, r, edge="e", move=None, label=""):
+    def U(self, y, h, r, edge="e", move=None, label=""):
 
         e = self.edges.get(edge, edge)
 
         w = self.edges["f"].spacing()
-        tw = x+2*w
-        th = y+w+e.spacing()
+        tw = y+2*w
+        th = h+w+e.spacing()
         if self.move(tw, th, move, True, label=label):
             return
 
         self.moveTo(w+r, w)
-        self.edges["f"](x-2*r)
+        self.edges["f"](y-2*r)
         self.corner(90, r)
-        self.edges["f"](y-r)
+        self.edges["f"](h-r)
         self.edgeCorner("f", e)
-        e(x)
+        e(y)
         self.edgeCorner(e, "f")
-        self.edges["f"](y-r)
+        self.edges["f"](h-r)
         self.corner(90, r)
 
         self.move(tw, th, move, label=label)
 
-    def Uwall(self, x, y, h, r, edges="ee", move=None, label=""):
+    def Uwall(self, y, h, x, r, edges="ee", move=None, label=""):
 
         e = [self.edges.get(edge, edge) for edge in edges]
 
         w = self.edges["F"].spacing()
         cl = r*math.pi/2
 
-        tw = 2*y + x - 4*(cl-r) + e[0].spacing() + e[1].spacing()
-        th = h + 2*w
+        tw = 2*h + y - 4*(cl-r) + e[0].spacing() + e[1].spacing()
+        th = x + 2*w
         if self.move(tw, th, move, True, label=label):
             return
 
         self.moveTo(e[0].spacing())
 
         for nr, flex in enumerate("XE"):
-            self.edges["F"](y-r)
-            if x-2*r > 0.1 * self.thickness:
+            self.edges["F"](h-r)
+            if y-2*r > 0.1 * self.thickness:
                 self.edges[flex](cl, h=th)
-                self.edges["F"](x-2*r)
+                self.edges["F"](y-2*r)
                 self.edges[flex](cl, h=th)
             else:
-                self.edges[flex](2*cl+x-2*r, h=th)
-            self.edges["F"](y-r)
+                self.edges[flex](2*cl+y-2*r, h=th)
+            self.edges["F"](h-r)
             if edges[0] in (self.edges["i"], self.edges["k"]):
                 # hinged lids, mimic ignor_widths
                 self.edgeCorner("e", e[nr])
-                e[nr](h + self.edges["F"].startWidth() +
+                e[nr](x + self.edges["F"].startWidth() +
                       self.edges["F"].endWidth())
                 self.edgeCorner(e[nr], "e")
             else:
                 self.edgeCorner("F", e[nr])
-                e[nr](h)
+                e[nr](x)
                 self.edgeCorner(e[nr], "F")
         self.move(tw, th, move, label=label)
 
     def render(self):
         x, y, h, r = self.x, self.y, self.h, self.radius
 
-        self.radius = r = min(r, x/2.0, y)
+        self.radius = r = min(r, y/2.0, h)
 
         self.edges["i"].settings.style = "flush_inset"
 
         _ = self.translations.gettext
         t1, t2, t3, t4 = self.topEdges(self.top_edge)
 
-        self.U(x, y, r, t1, move="right", label=_("left"))
-        self.U(x, y, r, t3, move="up", label=_("right"))
-        self.U(x, y, r, t3, move="left only")
-        self.Uwall(x, y, h, r, [t2, t4], move="up", label=_("wall"))
+        self.U(y, h, r, t1, move="right", label=_("left"))
+        self.U(y, h, r, t3, move="up", label=_("right"))
+        self.U(y, h, r, t3, move="left only")
+        self.Uwall(y, h, x, r, [t2, t4], move="up", label=_("wall"))
 
-        self.drawLid(h, x, self.top_edge)
-        self.lid(x, h, self.top_edge) # keep chest lid aligned with U
+        self.drawLid(x, y, self.top_edge)
+        self.lid(y, x, self.top_edge) # keep chest lid aligned with U

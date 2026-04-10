3d92a23bc486d1f8cb92a2517ee3d514070ad33c
Gustavo Schoenaker <ranaur2010@ranaur.net>
2026-04-10 13:01:37 -0300

    auto restart by script (the filechecker routine was not working in windows)

--- boxes/scripts/boxesserver.py
+++ boxes/scripts/boxesserver.py
@@ -75,11 +75,14 @@ class FileChecker(threading.Thread):
     def run(self) -> None:
         while not self._stopped:
             if not self.filesOK():
-                args = sys.argv.copy()
-                #args.insert(0,__file__)
-                al = f"python {' '.join(args)}"
-                print(f"Calling: {al}")
-                os.system(al)
+                #args = sys.argv.copy()
+                ##args.insert(0,__file__)
+                #al = f"python {' '.join(args)}"
+                #print(f"Calling: {al}")
+                #os.system(al)
+                print("Exiting")
+                import os
+                os._exit(1)
             time.sleep(1)
 
     def stop(self) -> None:

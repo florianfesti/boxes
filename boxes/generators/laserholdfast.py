cfc7e4c66762bc4cc065b050fa3e4d8925f6dc18
Martin Scharrer <martin.scharrer@web.de>
2026-05-04 14:21:44 +0200

    Fixes and improvements for LaserHoldfast
    * LaserHoldfast: Corrected overcut at start.
    * LaserHoldfast: Added `--num` argument to be able to produce multiple laser holdfasts.
    * LaserHoldfast: Added `--cutlength` argument to add a cut in the shaft.
    * LaserHoldfast: Changed lower-level code to use of polygonWall() with callback for the cutting of the shaft.

--- boxes/generators/laserholdfast.py
+++ boxes/generators/laserholdfast.py
@@ -21,6 +21,14 @@ class LaserHoldfast(Boxes):
 
     ui_group = "Part"
 
+    burn: float
+    x: float
+    h: float
+    hookheight: float
+    shaftwidth: float
+    cutlength: float
+    num: int
+
     def __init__(self) -> None:
         Boxes.__init__(self)
 
@@ -31,13 +39,60 @@ class LaserHoldfast(Boxes):
         self.argparser.add_argument(
             "--shaftwidth",  action="store", type=float, default=5.0,
             help="width of the shaft")
+        self.argparser.add_argument(
+            "--cutlength",  action="store", type=float, default=0.0,
+            help="length of cut in the shaft")
+        self.argparser.add_argument(
+            "--num",  action="store", type=int, default=1,
+            help="number of holdfasts")
 
     def render(self):
+        mx, my = self.offset()
+        for _ in range(self.num):
+            self.render_one()
+            self.moveTo(mx, my)
+
+    def offset(self):
+        """Calculate offset for next part"""
+        hh = self.hookheight
+        a = 30
+        r = self.x/math.radians(a)
+        d = r - r * math.cos(math.radians(a/2))
+        dd = hh - hh * math.cos(math.radians(a/2))
+        mx = hh + d - dd + self.spacing
+        my = self.shaftwidth + self.spacing
+        return mx, my
+
+    def render_one(self, move=None):
         # adjust to the variables you want in the local scope
         x, hh, h, sw = self.x, self.hookheight, self.h, self.shaftwidth
-        t = self.thickness
 
         a = 30
         r = x/math.radians(a)
+        dd = hh - hh * math.cos(math.radians(a/2))
 
-        self.polyline(hh+h, (180, sw/2), h, -90+a/2, 0, (-a, r), 0, (180, hh/2), 0, (a, r+hh), 0 , -a/2, sw-math.sin(math.radians(a/2))*hh , 90)
+        # Hook
+        self.polygonWall(
+            (
+                hh + h - dd, (180, sw / 2),
+                h, -90 + a / 2,
+                0, (-a, r),
+                0, (180, hh / 2),
+                0, (a, r + hh),
+                0, -a / 2,
+                sw - math.sin(math.radians(a / 2)) * hh, 90,
+            ),
+            edge='e',
+            move=move,
+            callback=self.cutcallback if self.cutlength else None
+        )
+
+    def cutcallback(self, num):
+        """Create cut in shaft if requested"""
+        if num == 1:
+            self.ctx.stroke()
+            with self.saved_context():
+                self.set_source_color(Color.INNER_CUT)
+                self.moveTo(-self.shaftwidth/2-self.burn, self.shaftwidth/2)
+                self.edge(self.cutlength)
+                self.ctx.stroke()
